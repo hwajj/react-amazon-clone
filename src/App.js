890e203de053f867e5950c7c76c71a5f7029af73
hwajj <azamwara@gmail.com>
2022-09-16 17:02:28 +0900

    Feat: Login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import { useEffect } from 'react';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
+
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(
+    () =>
+      //사용자 변화를 감지
+      auth.onAuthStateChanged((authUser) => {
+        console.log(authUser);
+        if (authUser) {
+          dispatch({
+            type: 'SET_USER',
+            user: authUser,
+          });
+        } else {
+          dispatch({
+            type: 'SET_USER',
+            user: null,
+          });
+        }
+      }),
+    []
+  );
   return (
     <Router>
       <div className='App'>
@@ -24,6 +49,14 @@ function App() {
               <>
                 <Header />
                 <Checkout />
+              </>
+            }
+          />
+          <Route
+            path='/login'
+            element={
+              <>
+                <Login />
               </>
             }
           />
